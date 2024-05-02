30b5825315f2488ad8cc69d87bca50ffca9779e2
Josh <Josh@DESKTOP-C9DI650>
2024-05-02 16:17:29 -0700

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string output = "";
+        int number;
+
+        for (number = 0; number <= 100; number++)
+        {
+
+
+            if (number % 5 == 0)
+            {
+                output = "Buzz";
+            }
+            if (number % 3 == 0)
+            {
+                output = "Fuzz";
+            }
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                output = "FizzBuzz";
+            }
+            if (number % 3 != 0 && number % 5 != 0)
+            {
+                output = Convert.ToString(number);
+            }
+
+
+            Console.WriteLine(output);
+
+        }
+        
+    }
+}
